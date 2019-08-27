b8ad3cc73a2bdc364ab510acf1310fbb08399480
Matthew Johnson <super66reaper@gmail.com>
2019-08-27 12:09:51 -0600

    Exercises 1 and 2 for Chapter 11

---
+++ Chapter11/ObjRunTime.java
@@ -0,0 +1,12 @@
+public class ObjRunTime {
+    public static void main (String[] args) {
+        String str = "Hello World";
+        String secondVar;
+
+        secondVar = new String("Hello My Son");
+        
+        System.out.println(str);
+        System.out.println(secondVar);
+
+    }
+}
---
+++ Chapter11/StringLength.java
@@ -0,0 +1,12 @@
+public class StringLength {
+    public static void main (String[] args) {
+        String str;
+        int    len;
+
+        str = new String( "Elementary,,    my dear Watson!!!!" );
+
+        len = str.length();
+
+        System.out.println("The length is: " + len);
+    }
+}
