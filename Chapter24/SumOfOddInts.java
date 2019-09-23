af7f7f4afffd5cf8fbb1be0096cdbc5cb7e67836
Matthew Johnson <super66reaper@gmail.com>
2019-09-23 12:38:45 -0600

    fixed name of exercise 2

--- Chapter24/SumOfOddInts.java
+++ Chapter24/SumOfOddInts.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SumOfDivisors {
+public class SumOfOddInts {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
         int n;
