23e29bf2fc83b3490d794679ca4fb0477e4e24d2
Matthew Johnson <super66reaper@gmail.com>
2019-09-23 12:54:21 -0600

    finish exercise 3 of chap 24

---
+++ Chapter24/SumOfDivisors.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOfDivisors {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n;
+        int sum = 0;
+        
+        System.out.println("What is n");
+        n = scan.nextInt();
+
+        int i;
+        for (i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+                System.out.println(i);
+            }
+        }
+        System.out.println("The sum of the divisors is: " + sum);
+    }
+}
