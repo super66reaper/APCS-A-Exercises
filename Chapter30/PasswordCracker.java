bf9c057acdc0b3d4c70b75c92e325e25d39c6633
Matthew Johnson <super66reaper@gmail.com>
2019-10-01 12:12:15 -0600

    finish exercise 5 of chap 30

--- Chapter30/PasswordCracker.java
+++ Chapter30/PasswordCracker.java
@@ -5,21 +5,22 @@ public class PasswordCracker {
         Scanner scan = new Scanner(System.in);
         Random random = new Random();
 
-        System.out.println("Enter a secret password 5 letters or less");
+        System.out.println("Enter a secret password 5 letters or less in all lowercase");
         String usrPass = scan.nextLine();
 
         String guess = "";
 
         String chara = "abcdefghijklmnopqrstuvwxyz ";
-
+        int attempts;
         int t = 0;
-        for (int attempts = 0; !guess.equals(usrPass); attempts++) {
+        for (attempts = 0; !guess.equals(usrPass); attempts++) {
             guess = "";
-            for(t = 0; t <= 5; t++) {
+            for(t = 0; t < 5; t++) {
 
                 guess += chara.charAt(random.nextInt(chara.length()));
             }
         }
-        System.out.println(guess);
+        System.out.println("Here is your password: " + guess);
+        System.out.println("It took " + attempts + " tries to guess it");
     }
 }
