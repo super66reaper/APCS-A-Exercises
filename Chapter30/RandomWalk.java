0525971020041334e663a5425009ab23a0146a7a
Matthew Johnson <super66reaper@gmail.com>
2019-10-02 13:11:08 -0600

    bug fixing in randomwalk

--- Chapter30/RandomWalk.java
+++ Chapter30/RandomWalk.java
@@ -2,14 +2,18 @@ import java.util.*;
 
 public class RandomWalk {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         Random random = new Random();
         double x = 0;
         double y = 0;
         double xAdd;
         double yAdd;
         double distance;
+        System.out.println("How many steps will the ant take?");
+        int stepTaken = scan.nextInt();
 
-        for (int t = 0; t <= 1000; t++) {
+
+        for (int t = 0; t <= stepTaken; t++) {
             xAdd = random.nextDouble() * 2 - 1;
             yAdd = random.nextDouble() * 2 - 1;
             x += xAdd;
